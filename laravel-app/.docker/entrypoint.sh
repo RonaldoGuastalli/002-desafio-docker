fcacc2d98bc92e4413d4f7d8afdde14c217f26b2
Ronaldo Guastalli <guastalli31@gmail.com>
2020-02-29 06:03:26 -0300

    arquivo do nginx.conf mais flexivel com variáveis de ambiente (dockerize)

--- laravel-app/.docker/entrypoint.sh
+++ laravel-app/.docker/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+dockerize -template ./.docker/app/.env:.env -wait tcp://db:3306 -timeout 40s
 
 composer install
+
 php artisan key:generate
 php artisan migrate
+
 php-fpm
